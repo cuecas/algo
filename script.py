0257bac0526562dc2958d2a0ae6a2ba2171210a2
carlosmorette <carlos.morette@outlook.com>
2024-05-26 22:40:57 -0300

    some improves

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import random
 
-BOARD_ROW_LENGTH = 10
-BOARD_COLUMN_LENGTH = 10
+BOARD_ROW_LENGTH = 7
+BOARD_COLUMN_LENGTH = 7
 
 MAX_TRIES_TO_PUT_WORD = BOARD_ROW_LENGTH * BOARD_COLUMN_LENGTH
 
@@ -20,25 +20,45 @@ WORDS_PATH = "words.txt"
 
 def new_game():
     board = build_board()
-    used_words = []
     positions = [] # only for cache
+    used_words = []
     words = load_words()
-    words_with_indexs = build_indexes(words)
 
     for word in words:
-        direction = get_new_direction()
-        match try_generate_random_position_to_put(board, direction, word):
-          case None:
-            board
-          case ("another_word", row, column, direction, obstacles_with_positions):
-            # find some word that match
-            return board
-          case (row, column):
+        match try_generate_random_position_to_put(board,  word):
+          case ("obstacles", obstacles, position, word, direction):
+            match find_match_word(board, obstacles, direction, position, words, used_words):
+              case ("word", match_word):
+                (row, column) = position
+                positions.append({word: position})
+                used_words.append(match_word)
+                board = put_on_board(board, row, column, direction, word)
+
+          case (row, column, direction):
             positions.append({word: (row, column)})
             used_words.append(word)
             board = put_on_board(board, row, column, direction, word)
 
     return (board, positions)
+
+def find_match_word(board: list, obstacles: list, direction: str, position: tuple, words: list, used_words: list):
+    if len(words) == 0:
+        return None
+
+    head = words[0]    
+    counter = 0
+    for (obstacle, index) in obstacles:
+        word_fit = is_word_fit(board, position, direction, head)
+        if (len(head) - 1) >= index and word_fit and head[index] and head not in used_words:
+            counter = counter + 1
+
+
+    if counter == len(obstacles):
+        ## TODO: fix
+        #return ("match_word", head)
+        return None
+    else:
+        return find_match_word(board, obstacles, direction, position, words[1:], used_words)
 
 def load_words() -> list:
     accumulator = []
@@ -47,17 +67,6 @@ def load_words() -> list:
             if word:
                 accumulator.append(word)
 
-    return accumulator
-
-def build_indexes(words: list):
-    return list(map(build_letters_with_positions, words))
-
-def build_letters_with_positions(word):
-    index = 0
-    accumulator = []
-    for l in word:
-        accumulator.append((l, index))
-        index = index + 1
     return accumulator
 
 def build_board() -> list:
@@ -98,33 +107,46 @@ def put_on_board(board: list, row: int, column: int, direction: int, word: str) 
             tail
         )
 
-def try_generate_random_position_to_put(board: list, direction: str, word: str, tries: list = []):
+def try_generate_random_position_to_put(board: list, word: str, tries: list = []):
     position = generate_new_position()
+    direction = get_new_direction()
     
-    if position in tries:
+    ## cache
+    if merge_position_with_direction(position, direction) in tries:
         return (
             None if len(tries) == MAX_TRIES_TO_PUT_WORD
-            else try_generate_random_position_to_put(board, direction, word, tries + [position])
+            else try_generate_random_position_to_put(
+                    board,
+                    word,
+                    tries
+            )
         )
 
     direction_is_used = direction_is_been_used(board, position, direction)
+    print("CURRENT DIRECTION: " + direction)
     word_fit = is_word_fit(board, position, direction, word)
     
     if not direction_is_used and word_fit:
          match get_obstacles(board, position, direction, word):
            case "no_obstacles":
-             return position
+             (row, column) = position
+             return (row, column, direction)
 
            case ("obstacles", obstacles):
-             print("obstacles: " + str(obstacles))
-             print("word: " + word)
-             #return ("obstacles", obstacles)
-             return position
+             return ("obstacles", obstacles, position, word, direction)
     else:
         return (
             None if len(tries) == MAX_TRIES_TO_PUT_WORD
-            else try_generate_random_position_to_put(board, direction, word, tries + [position])
+            else try_generate_random_position_to_put(
+                    board,
+                    word,
+                    tries + [merge_position_with_direction(position, direction)]
+            )
         )
+
+def merge_position_with_direction(position, direction):
+    (row, column) = position
+    return (row, column, direction)    
 
 def get_obstacles(board, position, direction, word):
     (row, column) = position
@@ -136,9 +158,8 @@ def get_obstacles(board, position, direction, word):
 
         if not next_row_is_exceeded(next_row) and not next_column_is_exceeded(next_column):
             if cell := board[next_row][next_column]:
-                obstacles.append({cell: index})
+                obstacles.append((cell, index))
 
-    ## Checar `obstacles`
     if len(obstacles) == 0:
         return "no_obstacles"
     else:
